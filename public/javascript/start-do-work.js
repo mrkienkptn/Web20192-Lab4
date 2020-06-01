c5e0b09a93a86c13841281ab3fd1b4cf94760a13
Ngai Kien <mrkienkptn@gmail.com>
2020-06-01 09:33:04 +0700

    multi to one sharing screen

--- public/javascript/start-do-work.js
+++ public/javascript/start-do-work.js
@@ -1,22 +1,23 @@
+let peer1 = new SimplePeer() // for employee share screen
 
-let peer2 = new SimplePeer()
-let peer1 = new SimplePeer()
 let remoteId = "00000"
-console.log("iiiiiiiiiii")
+
 
 $(".done button").click(function (e) {
     remoteId = $(this).attr("value")
+    let jobName = $(this).siblings('input').val().trim()
+    let workerName = $("#username p").html().trim()
     navigator.mediaDevices.getDisplayMedia({
         video: {
             frameRate: 100
         },
         audio: false
     }).then(stream => {
-        peer1 = new SimplePeer({ initiator: true, stream: stream })
+        peer1 = new SimplePeer({ initiator: true, trickle:false, stream: stream })
         //receive object from signaling server
         peer1.on('signal', data => {
-            console.log(" step1: remote id: "+ remoteId)
-            socket.emit('hand-shake1', {signal: data, remoteId: remoteId})
+            console.log(" step1: remote id: " + remoteId)
+            socket.emit('hand-shake1', { signal: data, remoteId: remoteId, jobName: jobName, workerName:workerName })
         })
 
         let video = document.getElementById("employee-screen")
@@ -28,49 +29,70 @@ $(".done button").click(function (e) {
             'right': '0'
         })
         video.play()
-        
+
     })
 })
 
-// rec signal from signaling server
-// send it to peer 2
+socket.on('hand-shake2', dataaa => {
+
+    let peerX = new SimplePeer()
+
+    console.log("Peeeeeeeeeeeeeeeeeeeeeeeeeeeeeer ")
+    // peerActive[i] = true
+    let signali = "kkk"
+
+    //rec signal that sent by peer 1 from server
+    // console.log(data)
+    let jobName = dataaa.jobName
+    let workerName = dataaa.workerName
+    let signal = dataaa.signal
+    peerX.signal(signal)
+    signali = dataaa.sender
 
 
+    // rec signal after connect to peer 1
+    // send this signal to peer 1
+    peerX.on('signal', data => {
+        console.log("XXXXXXXXXXXXXXXXXXXXX " + signali)
+        socket.emit('hand-shake3', { signal: data, remoteId: signali })
+    })
 
-let signal2 = "kkk"
+    // rec signal send by peer2 from server
+    // connect by this signal/
+    // done 3-hand-shake
+    peerX.on('stream', stream => {
+        let confir = confirm(workerName+" is starting do "+ jobName)
+        if (confir){
+            let videoDiv = document.createElement('div')
+            let titleDiv = document.createElement('div')
+            let title = workerName+" -is doing "+jobName
+            titleDiv.innerHTML = title
 
-//rec signal that sent by peer 1 from server
-socket.on('hand-shake2', data => {
-    console.log(data)
-    let signal = data.signal
-    peer2.signal(signal)
-    signal2 = data.sender    
+            videoDiv.appendChild(titleDiv)
+            let video = document.createElement('video')
+            let newWindow = window.open('', '_blank')
+            
+            video.setAttribute('width', '350')
+            video.setAttribute('height', '300')
+
+            video.setAttribute('controls', 'true')
+            video.srcObject = stream
+            video.play()
+            // document.getElementById('receive-sc-share').appendChild(video)
+            videoDiv.appendChild(video)
+            
+
+            
+            newWindow.document.body.append(videoDiv)
+            newWindow.document.title = 'Job'
+            
+        }
+    })
+   
+
 })
 
-// rec signal after connect to peer 1
-// send this signal to peer 1
-peer2.on('signal', data => {
-    console.log("XXXXXXXXXXXXXXXXXXXXX "+ signal2)
-    socket.emit('hand-shake3', {signal: data, remoteId: signal2} )
-})
-
-// rec signal send by peer2 from server
-// connect by this signal/
-// done 3-hand-shake
 socket.on('done-handshake', data => {
     console.log(data)
     peer1.signal(data)
 })
-
-peer2.on('stream', stream => {
-    let video = document.createElement('video')
-    video.setAttribute('width', '350')
-    video.setAttribute('height', '300')
-    
-    video.setAttribute('controls', 'true')
-    document.getElementById('receive-sc-share').appendChild(video)
-    
-    video.srcObject = stream
-    video.play()
-})
-
