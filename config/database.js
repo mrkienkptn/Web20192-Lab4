9b9e363a029cd36b38f409cf2bfda9175737d856
Ngai Kien <mrkienkptn@gmail.com>
2020-06-06 23:26:54 +0700

    ..

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    URI:"mongodb+srv://vanKien:vankien@crowd-jxkkr.mongodb.net/divuvila2?retryWrites=true&w=majority"
-    // URI: "mongodb://127.0.0.1:27017/divuvila=mongodb"
+    // URI:"mongodb+srv://vanKien:vankien@crowd-jxkkr.mongodb.net/divuvila2?retryWrites=true&w=majority"
+    URI: "mongodb://127.0.0.1:27017/divuvila=mongodb"
 }
